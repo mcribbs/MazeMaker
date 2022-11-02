aed2d718365adf5266f7f46c5e9ce424570143b4
Matt Cribbs <mcribbs@gmail.com>
2022-11-02 14:52:03 -0400

    skeleton page w/ canvas

---
+++ main.js
@@ -0,0 +1,10 @@
+function init() {
+	const canvas = document.getElementById('maze');
+	const ctx = canvas.getContext('2d');
+
+	ctx.fillStyle = "rgb(0, 200, 0)";
+    ctx.fillRect(10, 10, 50, 50);
+
+    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+    ctx.fillRect(30, 30, 50, 50);
+}
