49fd40b4595fcb04d19407a35da51201801d0d23
Matt Cribbs <mcribbs@gmail.com>
2022-11-02 20:51:24 -0400

    Setup grid

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
+var cols, rows;
+var w = 40;
+var grid = [];
+
 function setup() {
-	createCanvas(150,150)
+	createCanvas(400,400)
+    cols = floor(width/w);
+    rows = floor(height/w);
+
+    for (var j = 0; j < rows; j++) {
+        for (var i = 0; i < cols; i++) {
+            var cell = new Cell(i,j);
+            grid.push(cell);
+        }
+    }
 }
 
 function draw() {
-	fill(color('rgb(0, 200, 0)'));
-    rect(10, 10, 50, 50);
+    background(51);
+    for (var i = 0; i < grid.length; i++) {
+        grid[i].show();
+    }
+}
 
-    fill(color("rgba(0, 0, 200, 0.5)"));
-    rect(30, 30, 50, 50);
+function Cell (i,j) {
+    this.i = i;
+    this.j = j;
+    this.walls = [true, true, true, true];
+
+    this.show = function() {
+        var x = this.i*w;
+        var y = this.j*w;
+        stroke(255);
+        if (this.walls[0]) line(x,y,x+w,y);
+        if (this.walls[1]) line(x+w,y,x+w,y+w);
+        if (this.walls[2]) line(x+w,y+w,x,y+w);
+        if (this.walls[3]) line(x,y+w,x,y)
+    }
 }
