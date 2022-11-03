bf7b8a49e85a18f1aee0d55c2f8e1338c7694d3a
Matt Cribbs <mcribbs@gmail.com>
2022-11-02 23:08:27 -0400

    Remove walls

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function setup() {
     cols = floor(width/w);
     rows = floor(height/w);
 
-    frameRate(5);
+    //frameRate(5);
 
     for (var j = 0; j < rows; j++) {
         for (var i = 0; i < cols; i++) {
@@ -28,9 +28,11 @@ function draw() {
     }
 
     current.visited = true;
+    current.highlight();
     var next = current.checkNeighbors();
     if (next) {
         next.visited = true;
+        removeWalls(current,next);
         current = next;
     }
 }
@@ -69,6 +71,14 @@ function Cell (i,j) {
         }
     }
 
+    this.highlight = function() {
+        var x = this.i*w;
+        var y = this.j*w;
+        noStroke();
+        fill(0, 0, 255, 100);
+        rect(x,y,w,w);
+    }
+
     this.show = function() {
         var x = this.i*w;
         var y = this.j*w;
@@ -79,8 +89,28 @@ function Cell (i,j) {
         if (this.walls[3]) line(x,y+w,x,y);
 
         if (this.visited) {
+            noStroke();
             fill(255, 0, 255, 100);
             rect(x,y,w,w);
         }
+    }
+}
+
+function removeWalls(a,b) {
+    var x = a.i - b.i;
+    if (x === 1) {
+        a.walls[3] = false;
+        b.walls[1] = false;
+    } else if (x === -1) {
+        a.walls[1] = false;
+        b.walls[3] = false;
+    }
+    var y = a.j - b.j;
+    if (y === 1) {
+        a.walls[0] = false;
+        b.walls[2] = false;
+    } else if (y === -1) {
+        a.walls[2] = false;
+        b.walls[0] = false;
     }
 }
